ee3d3b5b6b7bc9c1bb3163c1cbe54e28ad7008d4
Connor Buckley <cbuckley@usc.edu>
2018-11-10 14:24:55 -0800

    Add Timer Function

--- final-project/WebContent/js/socket.js
+++ final-project/WebContent/js/socket.js
@@ -3,6 +3,27 @@
  * needed for autosave
  */
 // wait until page is loaded
-$(document).ready(function () {
+//setup before functions
+var typingTimer;                //timer identifier
+var doneTypingInterval = 5000;  //time in ms (5 seconds)
+var socket;
+
+//on keyup, start the countdown
+$('#text-area').keyup(function(){
+    clearTimeout(typingTimer);
+    if ($('#text-area').val()) {
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    }
+});
+
+//user is "finished typing," do something
+function doneTyping () {
+    //do something
+	socket.send("message");
 	
+	
+}
+
+$(document).ready(function () {
+	socket = new WebSocket("ws://localhost:8080/final-project/ws");
 });
