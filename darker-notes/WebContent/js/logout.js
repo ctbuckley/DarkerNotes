f71d98d80d84ae17ed39c04f9c9dc98ddb24e815
Alexander Gednov <gednov@usc.edu>
2018-11-11 19:56:05 -0800

    added logout function, cleaned up index.html

---
+++ darker-notes/WebContent/js/logout.js
@@ -0,0 +1,20 @@
+/*
+ * Logout user function
+ */
+
+function logout() {
+	console.log("logging out " + sessionStorage.getItem("name"))
+	
+	// clear sessionStorage
+	sessionStorage.clear();
+	sessionStorage.setItem("currentFileID", -1);
+	sessionStorage.setItem("fileName", "Default File");
+	sessionStorage.setItem("signedin", false);
+	
+	// hide elements, put back into guest mode
+	$("#signin-button").toggleClass("d-none");
+	$("#signout-button").toggleClass("d-none");
+	$("#share-button").toggleClass("d-none");
+	$("#sidebarCollapse").toggleClass("d-none");
+	
+}
